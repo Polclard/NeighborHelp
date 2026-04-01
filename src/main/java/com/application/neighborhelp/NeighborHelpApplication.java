00f9f356325fcf0eead97f72a100558903b62f4d
alenjangelov <alenjang007@gmail.com>
2026-04-01 18:12:08 +0200

    Initial commit

---
+++ src/main/java/com/application/neighborhelp/NeighborHelpApplication.java
@@ -0,0 +1,13 @@
+package com.application.neighborhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeighborHelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/neighborhelp/NeighborHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.neighborhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborHelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
